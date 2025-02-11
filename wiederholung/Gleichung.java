a9f1a36e24b80c7512f857dfdf2a5ba5a72f6020
Markus Schabel <markus.schabel@gmail.com>
2025-02-11 20:22:07 +0100

    Berücksichtigung von 0

--- wiederholung/Gleichung.java
+++ wiederholung/Gleichung.java
@@ -4,12 +4,17 @@ public class Gleichung {
         double b = 2.0;
         double c = 1.0;
 
-        
-        double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-        double x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        if ( a == 0) {
+            double x = -c / b;
 
+            System.out.println("x = " + x);
+        } else {
+            double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+            double x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);    
+        }
         
-        System.out.println("x1 = " + x1);
-        System.out.println("x2 = " + x2);
     }
 }
