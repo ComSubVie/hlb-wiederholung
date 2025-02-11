8cb76f4d790edb33c0950c16bd5008c9f8d752fe
Markus Schabel <markus.schabel@gmail.com>
2025-02-11 20:08:27 +0100

    Update Gleichung.java

--- wiederholung/Gleichung.java
+++ wiederholung/Gleichung.java
@@ -5,6 +5,8 @@ public class Gleichung {
         double c = 1.0;
         double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
         double x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+        
         System.out.println("x1 = " + x1);
         System.out.println("x2 = " + x2);
     }
